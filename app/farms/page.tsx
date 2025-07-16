0ed5e7c6d59a9bc3a2240919ff5a1f08a29cbd02
Manohar <manohargajangi1037@gmail.com>
2025-07-16 11:27:42 +0530

    page fix

---
+++ app/farms/page.tsx
@@ -0,0 +1,13 @@
+import Navbar from "@/components/navbar";
+import React from "react";
+
+const farms = () => {
+  return (
+    <>
+      <Navbar></Navbar>
+      <div className="min-h-screen bg-[#151A2F] px-4 py-10 flex justify-center"></div>
+    </>
+  );
+};
+
+export default farms;
